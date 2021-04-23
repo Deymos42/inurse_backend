d8136473240820dfc91bb177c98a6691b3ea0615
David Muñoz <dmbdavid97@gmail.com>
2021-04-23 12:49:10 +0200

    production v1.2

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,10 @@ RUN chmod +x /scripts/*
 
 
 RUN mkdir /var/log/django
-RUN touch /var/log/django/APIapp.log
+RUN touch /var/log/django/myapp.log
 RUN adduser -D user
 RUN chown -R user:user /app
-RUN chmod -R 777 /var/log/django/APIapp.log
+RUN chmod -R 777 /var/log/django/myapp.log
 RUN chmod -R 755 /app/APIapp
 RUN rm -rf /app/APIapp/static/APIapp
 
--- inurse/API/settings.py
+++ inurse/API/settings.py
@@ -42,13 +42,52 @@ else:
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+             # But the emails are plain text by default - HTML is nicer
+            'include_html': True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/django/myapp.log'
+        },
+    },
+    'loggers': {
+        # Again, default Django configuration to email unhandled exceptions
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        # Your own app - this assumes all your logger names start with "myapp."
+        'myapp': {
+            'handlers': ['logfile'],
+            'level': 'WARNING', # Or maybe INFO or DEBUG
+            'propagate': False
+        },
+    },
+}
 
 # Application definition
 
